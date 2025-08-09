97c42d73650d28ec1157739e4d56e9a8e94dc25f
GaDiFat <132307289+GaDiFat@users.noreply.github.com>
2025-08-08 21:32:23 -0600

    Actualización de requerimientos y despliegue visual HL ↔ LL

--- src/components/HLComponent.js
+++ src/components/HLComponent.js
@@ -8,15 +8,15 @@ const hlReqs = [
     llLinks: [
       {
         id: "LL01",
-        text: "Asignar Verdadero si la bandera de posición incorrecta es Verdadera > 1s.",
+        text: "El sistema debe asignar el valor de Verdadero a la bandera de retraso de respuesta si la bandera de posición incorrecta es Verdadera durante más de 1 segundo.",
       },
       {
         id: "LL02",
-        text: "Asignar Falso si la bandera de posición incorrecta es Verdadera > 3s.",
+        text: "El sistema debe asignar el valor de Falso a la bandera de retraso de respuesta si la bandera de posición incorrecta es Verdadera por más de 3 segundos.",
       },
       {
         id: "LL03",
-        text: "Asignar Falso si la bandera de posición incorrecta es Falsa.",
+        text: "El sistema debe asignar el valor de Falso a la bandera de retraso de respuesta si la bandera de posición incorrecta es Falsa.",
       },
     ],
   },
@@ -24,20 +24,36 @@ const hlReqs = [
     id: "HL02",
     text: "El sistema debe determinar el valor de la bandera de posición incorrecta.",
     llLinks: [
-      { id: "LL04", text: "Calcular error de posición = comando - estimada." },
-      { id: "LL05", text: "Asignar Verdadero si |error| > 1°." },
-      { id: "LL06", text: "Asignar Falso si |error| ≤ 1°." },
+      {
+        id: "LL04",
+        text: "El sistema debe calcular el error de posición como error de posición=comando de posición-posición estimada",
+      },
+      {
+        id: "LL05",
+        text: "El sistema debe asignar el valor de Verdadero a la bandera de posición incorrecta si el valor absoluto del error de posición es mayor a un grado.",
+      },
+      {
+        id: "LL06",
+        text: "El sistema debe asignar el valor de Falso a la bandera de posición incorrecta si el valor absoluto del error de posición es menor o iguial a un grado.",
+      },
     ],
   },
   {
     id: "HL03",
-    text: "El sistema debe determinar el valor de la bandera de estado inválido.",
+    text: "El sistema debe determinar el valor de la bandera de estado invalido",
     llLinks: [
       {
         id: "LL07",
-        text: "Asignar Verdadero si posición estimada > 90° o < 0°.",
+        text: "El sistema debe asignar el valor de Verdadero a la bandera de estado invalido si la posición estimada es mayor a 90 grados o menor a 0 grados.",
       },
-      { id: "LL08", text: "Asignar Verdadero si WoW = Falso y vuelo = Falso." },
+      {
+        id: "LL08",
+        text: "El sistema debe asignar el valor de Verdadero a la bandera de estado invalido si el estado de WoW es Falso y el estado de vuelo es Falso",
+      },
+      {
+        id: "LL09",
+        text: "El sistema debe asignr el valor de Verdadero a la bandera de inconsistencia de peso si el estado de WoW es Falso y el estado de vuelo es Falso.",
+      },
     ],
   },
   {
@@ -45,37 +61,49 @@ const hlReqs = [
     text: "El sistema debe evaluar el estado del tren.",
     llLinks: [
       {
-        id: "LL09",
-        text: "Asignar 1 al estado de tren si estado inválido = Falso y posición > 0°.",
+        id: "LL10",
+        text: "El sistema debe asignar el valor de 1 al estado de tren si la bandera de estado invalido es Falsa y la posición estimada es mayor a 0 grados.",
       },
       {
-        id: "LL10",
-        text: "Asignar 0 al estado de tren si estado inválido = Falso y posición = 0°.",
+        id: "LL11",
+        text: "El sistema debe asignar el valor de 0 al estado de tren si la bandera de estado invalido es Falsa y la posición estimada es igual a 0 grados.",
       },
     ],
   },
   {
     id: "HL05",
-    text: "El sistema debe calcular el contador de estado inválido como tiempo durante el cual se ha mantenido en Verdadero la bandera de estado inválido.",
+    text: "El sistema calcular el contador de estado invalido como tiempo durante el cual se ha mantenido en Verdadero la bandera de estado invalido.",
     llLinks: [
       {
-        id: "LL11",
-        text: "Activar bandera de contador si estado inválido = Verdadero y bandera = Falsa.",
+        id: "LL12",
+        text: "El sistema debe asignar el valor de Verdadero a la bandera de contador de estado invalido si la bandera de contador de estado invalido es Falsa y la bandera de estado invalido es Verdadera.",
       },
       {
-        id: "LL12",
-        text: "Incrementar contador en 10 si ambas banderas = Verdadero.",
+        id: "LL13",
+        text: "El sistema debe incrementar en un valor de 10 el contador de estado invalido si la bandera de estado invalido es Verdadera y la bandera de contador de estado invalido es Verdadera.",
       },
-      { id: "LL13", text: "Asignar 0 si estado inválido = Falso." },
-      { id: "LL14", text: "Desactivar bandera si estado inválido = Falso." },
+      {
+        id: "LL14",
+        text: "El sistema debe asignar el valor de 0 al contador de estado invalido si la bandera de estado invalido es Falsa.",
+      },
+      {
+        id: "LL15",
+        text: "El sustema debe asignar el valor de Falso a la bandera de contador de estado invalido si la bandera de estado invalido es Falsa.",
+      },
     ],
   },
   {
     id: "HL06",
-    text: "El sistema debe determinar el valor de la bandera de obsolescencia de datos.",
+    text: "El sistema debe determinar el valor de la bandera de obsolesencia de datos.",
     llLinks: [
-      { id: "LL15", text: "Asignar Verdadero si contador > 100." },
-      { id: "LL16", text: "Asignar Falso si contador ≤ 100." },
+      {
+        id: "LL16",
+        text: "El sistema debe asignar el valor de Verdadero a la bandera de obsolesencia de datos si el contador de estado invalido es mayor a 100.",
+      },
+      {
+        id: "LL17",
+        text: "El sistema debe asignar el valor de Falso a la bandera de obsolesencia de datos si el contador de estado invalido es menor o igual a 100.",
+      },
     ],
   },
   {
@@ -83,22 +111,21 @@ const hlReqs = [
     text: "El sistema debe asignar el valor de Verdadero a la bandera de alarma sonora durante una duración máxima de 30 segundos si el estado de despegue y el estado de tren están y han permanecido con valor de Verdadero y 1 respectivamente durante más de 10 segundos.",
     llLinks: [
       {
-        id: "LL17",
-        text: "Asignar Verdadero si estado de despegue y tren = Verdadero y 1 por > 10s.",
-      },
-      {
         id: "LL18",
-        text: "Asignar estado de despegue = Verdadero si velocidad > 90 nudos y altitud > 500 pies.",
+        text: "El sistema debe asignar el valor de Verdadero al estado de despegue si la velocidad es mayor a 90 nudos y la altitudo mayor a 500 pies.",
       },
       {
         id: "LL19",
-        text: "Incrementar contador en 10 si condiciones se cumplen y contador < 10000.",
+        text: "El sistema debe incrementar en un valor de 10 el contador para alarma sonora si el estado de despegue es Verdadero, el estado de tren es 1 y el valor del contador para alarma sonora es menor a 10000.",
       },
       {
         id: "LL20",
-        text: "Incrementar contador en 10 si contador = 10000 y < 30000.",
+        text: "El sistema debe incrementar en un valor de 10 el contador de alarma sonora si el estado de despegue es Verdadero, el estado de tren es 1, el valor del contador par alarma sonora es 10000 y el valor del contador de alarma sonora es menor a 30000.",
       },
-      { id: "LL21", text: "Asignar Verdadero si contador > 0." },
+      {
+        id: "LL21",
+        text: "El sistema debe asignar el valor de Verdadero a la bandera de alarma sonora si el contador de alarma sonora es mayor a 0.",
+      },
     ],
   },
   {
@@ -107,15 +134,11 @@ const hlReqs = [
     llLinks: [
       {
         id: "LL22",
-        text: "Asignar Falso si duración = 30000s o despegue = Falso o tren = 0.",
+        text: "El sistema debe asignar el valor de 0 al contador para alarma sonora si el estado de despegue es Falso o el estado de tren no es 1 o el contador de alarma sonora es igual a 30000.",
       },
       {
         id: "LL23",
-        text: "Asignar 0 al contador si despegue = Falso o tren ≠ 1 o contador = 30000.",
-      },
-      {
-        id: "LL24",
-        text: "Asignar 0 al contador si despegue = Falso o tren ≠ 1.",
+        text: "El sistema debe asignar el valor de 0 al contador de alarma sonora si el estado de despegue es Falso o el estado de tren no es 1.",
       },
     ],
   },
@@ -123,39 +146,47 @@ const hlReqs = [
     id: "HL09",
     text: "El sistema debe contar con un interruptor físico dedicado para habilitar el modo de prueba (testing) en condiciones de operación en tierra.",
     llLinks: [
-      { id: "LL25", text: "Incorporar un interruptor físico de tipo toggle." },
+      {
+        id: "LL24",
+        text: "El sistema debe incorporar un interruptor físico de tipo toggle.",
+      },
     ],
   },
   {
     id: "HL10",
     text: "El sistema debe mostrar alertas visuales a la tripulación a través de diodos emisores de luz (LEDs), activando el LED correspondiente de acuerdo con el tipo de alerta.",
     llLinks: [
-      { id: "LL26", text: "LED rojo estado inválido: 300 lúmenes, 694 nm." },
-      { id: "LL27", text: "LED verde estado válido: 300 lúmenes, 495 nm." },
-      { id: "LL28", text: "LED rojo inconsistencia de peso." },
+      {
+        id: "LL25",
+        text: "El diodo de estado inválido debe tener una intensidad de 300 lúmenes y longitud de onda de 694 nm(Color rojo).",
+      },
+      {
+        id: "LL26",
+        text: "El diodo de estado valido debe tener una intensidad de 300 lúmenes y longitud de onda de 495 nm (Color verde).",
+      },
+      {
+        id: "LL27",
+        text: "El sistema debe contar con un diodo de inconsistencia de peso que se activara en Rojo cuando se tenga inconsistencia de peso.",
+      },
+      {
+        id: "LL28",
+        text: "El sistema debe encender un LED verde identificado como “Diodo WOW” cuando la bandera de inconsistencia del estado Weight-On-Wheels (WOW) sea falsa.",
+      },
       {
         id: "LL29",
-        text: "LED verde 'Diodo WOW' si inconsistencia WOW = Falsa.",
+        text: "El sistema debe encender un LED rojo identificado como “Diodo WOW” cuando la bandera de inconsistencia del estado Weight-On-Wheels (WOW) sea verdadera.",
       },
       {
         id: "LL30",
-        text: "LED rojo 'Diodo WOW' si inconsistencia WOW = Verdadera.",
+        text: "El sistema debe encender un LED verde identificado como “Diodo de Salud de Datos” cuando la bandera de obsolescencia de datos sea falsa.",
       },
       {
         id: "LL31",
-        text: "LED verde 'Salud de Datos' si obsolescencia = Falsa.",
+        text: "El sistema debe encender un LED rojo identificado como “Diodo de Salud de Datos” cuando la bandera de obsolescencia de datos sea verdadera.",
       },
       {
         id: "LL32",
-        text: "LED rojo 'Salud de Datos' si obsolescencia = Verdadera.",
-      },
-      {
-        id: "LL33",
-        text: "LED verde 'Posición' si todas las banderas = Falsa.",
-      },
-      {
-        id: "LL34",
-        text: "LED rojo 'Posición' si alguna bandera = Verdadera.",
+        text: "El sistema debe encender un LED verde identificado como “Diodo de Posición” cuando las siguientes tres condiciones se cumplan simultáneamente: La bandera de posición incorrecta sea falsa.",
       },
     ],
   },
